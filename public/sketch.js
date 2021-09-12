deff044ad3402ccbbbe20ed83e66f17b527279a4
galo.canizares@gmail.com <galo.canizares@gmail.com>
2021-09-12 16:53:02 -0500

    test

--- public/sketch.js
+++ public/sketch.js
@@ -1,26 +1,65 @@
-var myslider;
+//BUILDING WEB-BASED DRAWING INSTRUMENTS TEMPLATE
+//BY GALO CANIZARES
+
+var lowresCanvas;
+var lineStorage = [];
+var tempLineData = {
+  x1: 0,
+  y1: 0,
+  x2: 0,
+  y2: 0
+}
+var remapX;
+var remapY;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  myslider=createSlider(100,2000,100)
-  myslider.position(10,10)
-  background(240, 152, 98);
+  createCanvas(windowWidth, windowHeight);
+  lowresCanvas = createGraphics(32, 32);
+  noSmooth();
+  lowresCanvas.noSmooth();
+
+  remapX = map(mouseX, 0, windowWidth, 0, 32);
+  remapY = map(mouseY, 0, windowHeight, 0, 32);
 }
 
 function draw() {
-  //background(240, 152, 98);
-  var val = myslider.value();
+  background(220);
+  lowresCanvas.background(220);
+  for(var i = 0; i < lineStorage.length; i++) {
+    lineStorage[i].render();
+  }
+  if(mouseIsPressed) {
+    lowresCanvas.line(tempLineData.x1, tempLineData.y1, map(mouseX, 0, windowWidth, 0, 32), map(mouseY, 0, windowHeight, 0, 32));
+  }
+  image(lowresCanvas, 0, 0, windowWidth, windowHeight);
+}
 
-  fill(mouseY, random(250), mouseY);
-  //stroke(65, 145, 140);
-  noStroke();
-  push();
-  translate(mouseX-200, 0, mouseY);
-  rotateZ(mouseX/100);
-  rotateY(mouseY/100);
-  torus(val/4);
+function myLine(startX, startY, endX, endY, r, g, b) {
+  this.x1 = startX;
+  this.y1 = startY;
+  this.x2 = endX;
+  this.y2 = endY;
+  this.r = r;
+  this.g = g;
+  this.b = b;
 
+  this.render = function() {
+    lowresCanvas.stroke(this.r, this.g, this.b);
+    lowresCanvas.strokeWeight(1);
+    lowresCanvas.line(this.x1, this.y1, this.x2, this.y2);
+  }
+}
 
-  pop();
+function mousePressed() {
+
+  tempLineData.x1 = map(mouseX, 0, windowWidth, 0, 32);
+  tempLineData.y1 = map(mouseY, 0, windowHeight, 0, 32);
+}
+
+function mouseReleased() {
+  tempLineData.x2 = map(mouseX, 0, windowWidth, 0, 32);;
+  tempLineData.y2 = map(mouseY, 0, windowHeight, 0, 32);
+  lineStorage.push(new myLine(tempLineData.x1, tempLineData.y1, tempLineData.x2, tempLineData.y2, random(255), random(255), random(255)));
 }
 
 function windowResized() {
